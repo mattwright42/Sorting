9a054198edcf67e15ba18785be0cc29e53ef4202
Matthew Wright <matthewbwright@gmail.com>
2019-02-18 14:12:50 -0700

    completes the Insertion Sort function

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -26,8 +26,20 @@ print(my_arr)
 
 # TO-DO: implement the Insertion Sort function below
 def insertion_sort(arr):
-
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
     return arr
+
+
+arr = [12, 11, 13, 5, 6]
+insertion_sort(arr)
+for i in range(len(arr)):
+    print("%d" % arr[i])
 
 
 # STRETCH: implement the Bubble Sort function below
